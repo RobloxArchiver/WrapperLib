66cb93c9a53c0a8332ab821812d2c8c6603265d1
Zer0D_ay <95448029+RobloxArchiver@users.noreply.github.com>
2022-03-09 12:12:49 -0500

    Update Module.lua

--- games/anomic-revamp/Module.lua
+++ games/anomic-revamp/Module.lua
@@ -18,9 +18,10 @@ Such as sending a notification, Buying something, or clicking something.
 ]]
 
 -- summary: sends a notification
--- 
-function NotificationHandler(keepnil, Message, Color)
-    require(game.ReplicatedStorage.NotificationHandler):SendNotification(keepnil, Message, Color)
+-- Message: <string>
+-- Color: Color3.fromRGB(vec3) or Color3.new(vec3)
+function WrapperLib.NotificationHandler(Message, Color)
+    require(game.ReplicatedStorage.NotificationHandler):SendNotification(nil, Message, Color)
 end
 
 return WrapperLib
