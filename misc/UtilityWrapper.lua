84487ef253f00f735d6605b54f216c06acf59423
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-03-25 09:49:20 -0400

    Added `UW.GetRBXVer`

--- misc/UtilityWrapper.lua
+++ misc/UtilityWrapper.lua
@@ -13,7 +13,12 @@ end
 
 -- Normal roblox noti, smaller cleaner code. 
 function UW.SendNotification(title, text, duration)
-    game.StarterGui:SetCore("SendNotification", {Title = title; Text = text; Duration = duration;})
+	game.StarterGui:SetCore("SendNotification", {Title = title; Text = text; Duration = duration;})
+end
+
+-- Gets roblox ver, if theres another way sorry, probably wont work right now, wil ltest when I get home :)
+function UW.GetRBXVer()
+	loadstring(game:HttpGet("https://setup.roblox.com/version"))()
 end
 
 return UW
