10658e1a01cd521b4de467a637ce52d94b7c94f5
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-03-28 08:05:48 -0400

    Update UtilityWrapper.lua

--- misc/UtilityWrapper.lua
+++ misc/UtilityWrapper.lua
@@ -1,19 +1,23 @@
--- // Wont be documented! Simple and easy to read! 
+-- // Wont be documented! Simple and easy to read!
 
 local UW = {}
 
 -- Will be used in future scripts
 function UW.ServerHopper(limit)
-	for i,v in pairs(game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=" .. limit)).data) do
-		if v.playing ~= v.maxPlayers then
-			game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, v.id)
-		end
-	end
+    for i,v in pairs(game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=" .. limit)).data) do
+        if v.playing ~= v.maxPlayers then
+            game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, v.id)
+        end
+    end
 end
 
--- Normal roblox noti, smaller cleaner code. 
+-- Normal roblox noti, smaller cleaner code.
 function UW.SendNotification(title, text, duration)
-	game.StarterGui:SetCore("SendNotification", {Title = title; Text = text; Duration = duration;})
+    game.StarterGui:SetCore("SendNotification", {
+        Title = title;
+        Text = text;
+        Duration = duration;
+    })
 end
 
 -- Depricated until further notice
