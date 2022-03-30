8dbd91ea394d1c0e303d5fef9584b23a7640f891
Uptight <RobloxArchiver@protonmail.com>
2022-03-30 00:45:22 -0400

    Added `Teleports`

--- games/bubble-gum-simulator/Remotes.lua
+++ games/bubble-gum-simulator/Remotes.lua
@@ -1,4 +1,6 @@
 local WL = {}
+
+--> NetworkRemoteEvent
 local NetworkEvent = game:GetService("ReplicatedStorage").NetworkRemoteEvent
 
 function WL.BlowBubble()
@@ -14,8 +16,22 @@ function WL.Teleport(location)
     NetworkEvent:FireServer("Teleport", location)
 end
 
+--> Place: "EarthFlavors"
+--> ItemType: "Flavors", "Gum", "Faces"
 function WL.BuyShopItem(place, ItemType, item)
     NetworkEvent:FireServer("BuyShopItem", place, ItemType, item)
+end
+
+function WL.CollectChest(island)
+    NetworkEvent:FireServer("CollectChestReward", island)
+end
+
+function WL.TeleportToADoor(island)
+    NetworkEvent:FireServer("TeleportToADoor", island)
+end
+
+function WL.TeleportToCheckpoint(island)
+    NetworkEvent:FireServer("TeleportToCheckpoint", island)
 end
 
 return WL
---
+++ games/bubble-gum-simulator/dumps/README.md
@@ -0,0 +1,7 @@
+# Paths 
+```lua
+game:GetService("ReplicatedStorage").Assets.Modules.ItemDataService.ShopModule
+```
+```lua
+game:GetService("ReplicatedStorage").Assets.Modules.ItemDataService
+```
