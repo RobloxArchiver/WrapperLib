3c0d305bb89423e2a280794bdd91dbe06dbbe0ef
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-05-11 09:50:22 -0400

    Added Documentation Writer

--- misc/MakeDoc.lua
+++ misc/MakeDoc.lua
@@ -1,1 +1,30 @@
+local mode = "print" --> Method Used to Print out.
 
+local function _write(text)
+    if string.lower(mode) == "print" then
+        print(text);
+    end;
+end;
+
+--[[
+    isTitle --> Wether its a title or not.
+    functionName --> Sets Function used. 
+    parent --> Sets parent
+    type --> Sets type
+    offset --> sets offset
+    desc --> sets description
+    snippet --> sets snippet
+]]
+
+function MakeDoc(self)
+    _write("## " .. self.functionName);
+    _write("")
+    _write("* Parent: `" .. self.parent .. "`");
+    _write("* Type: `" .. self.type .. "`");
+    _write("* Offset: `" .. self.offset .. "`");
+    _write("* Description: " .. self.desc);
+    _write("")
+    _write("```lua");
+    _write("    " .. self.snippet);
+    _write("```")
+end;
