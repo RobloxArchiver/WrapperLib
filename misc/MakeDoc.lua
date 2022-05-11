39473c16845fcd3885ee6c5e7d89e4f3e8038f8d
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-05-11 09:58:45 -0400

    Added `rconsoleprint` support

--- misc/MakeDoc.lua
+++ misc/MakeDoc.lua
@@ -1,8 +1,17 @@
+local console = loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxArchiver/Console/main/src/main.lua"))():Init(true, "MakeDoc");
 local mode = "print" --> Method Used to Print out.
 
 local function _write(text)
     if string.lower(mode) == "print" then
         print(text);
+    elseif string.lower(mode) == "rconsoleprint" then
+        if rconsoleprint then
+            if (identifyexecutor() or getexecutorname()) == "ScriptWare" then
+                rconsolecreate();
+            end;
+            
+            console.log(text);
+        end;
     end;
 end;
 
